8d468abcfd6aa9307c0f592154fc7b43526c3771
alexw10 <alexw10@live.com>
2018-09-03 19:36:10 -0500

    feat(graphql): setup graphql and get playground running

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,46 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { graphqlExpress, ApolloServer } from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
+import axios from 'axios';
+
+const TYPEDEFS = gql`
+  type Query {
+    test_query: Test
+  }
+  type Test {
+    test_field_1: String
+    test_field_2: Int
+    test_field_3: Boolean
+  }
+`;
+
+const RESOLVERS = {
+  Query: {
+    test_query: (parent, args) => {
+      return axios
+        .get(`www.apiurl.com/people`)
+        .then(response => response.data)
+        .catch(error => console.log(error));
+    }
+  }
+};
+
+const SERVER = new ApolloServer({
+  typeDefs: TYPEDEFS,
+  resolvers: RESOLVERS,
+  playground: {
+    endpoint: `http://localhost:4000/graphql`,
+    settings: {
+      'editor.theme': 'light'
+    }
+  }
+});
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
+SERVER.applyMiddleware({
+  app: app
 });
 
 app.listen(4000, () => {
